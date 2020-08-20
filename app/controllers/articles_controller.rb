db36dafb3736d22ddd05d4c8a660b1a28eae7337
Uttej Kumar <uttejkumarponugoti@gmail.com>
2020-08-20 12:03:08 +0530

    creating new articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,4 +5,12 @@ end
 def index
     @articles=Article.all
 end
+def new
+
+end
+def create
+@article=Article.new(params.require(:article).permit(:title,:description))
+@article.save
+redirect_to @article
+end
 end
