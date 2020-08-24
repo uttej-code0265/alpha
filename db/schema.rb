cb2e5292702d25d2de47a2b42c355be377b8396b
Uttej Kumar <uttejkumarponugoti@gmail.com>
2020-08-24 18:35:48 +0530

    users action

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_24_103026) do
+ActiveRecord::Schema.define(version: 2020_08_24_114553) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_08_24_103026) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
